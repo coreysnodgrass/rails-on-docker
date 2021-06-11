eb6de09223f834fef83b8caa61b396bbef145052
Corey Snodgrass <corey.snodgrass@gmail.com>
2021-06-11 15:21:55 -0400

    Add .env variables to use in bash scripts

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,13 @@ rm -f /myapp/tmp/pids/server.pid
 
 # Check if rails app exists yet
 if [ ! -d "/myapp/app" ]; then
-    rails new . --skip-gemfile --skip-git --database=postgresql &&
-    cp /myapp/init/database.yml /myapp/config/database.yml
+
+    if [ -f .env ]; then
+        export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
+    fi
+    
+    rails new . --skip-gemfile --skip-git --database=$DATABASE_TYPE && 
+        cp /myapp/init/database.yml /myapp/config/database.yml
 fi
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
