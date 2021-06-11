f296fcc43086cdca497d9c15b91753ec8791de05
Corey Snodgrass <corey.snodgrass@gmail.com>
2021-06-11 17:43:41 -0400

    Change the way env variables are loaded and get the right path

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,15 +4,13 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
-# Check if rails app exists yet
+# Check if rails app exists and .env exists, load and exec
 if [ ! -d "/myapp/app" ]; then
-
-    if [ -f .env ]; then
-        export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
-    fi
-    
-    rails new . --skip-gemfile --skip-git --database=$DATABASE_TYPE && 
+    if [ -f "/myapp/.env" ]; then
+        export $(grep -v '^#' /myapp/.env | xargs) &&
+        rails new . --skip-gemfile --skip-git --database=$DATABASE_TYPE && 
         cp /myapp/init/database.yml /myapp/config/database.yml
+    fi
 fi
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
