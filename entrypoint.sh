db59dc7d99fb1d0c8566b2aa9ec7fa689922cad9
Corey Snodgrass <corey.snodgrass@gmail.com>
2021-06-12 03:03:23 -0400

    Fix CRLF endlines that was totally hosing the program and add the rake command to create the database, it seems to need it

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,9 @@ rm -f /myapp/tmp/pids/server.pid
 
 # Check if rails app exists and .env exists, load and exec
 if [ ! -d "/myapp/app" ]; then
-    if [ -f "/myapp/.env" ]; then
-        export $(grep -v '^#' /myapp/.env | xargs) &&
-        rails new . --skip-gemfile --skip-git --database=$DATABASE_TYPE && 
-        cp /myapp/init/database.yml /myapp/config/database.yml
-    fi
+    rails new . --skip-gemfile --skip-git --database=${DATABASE_TYPE} && 
+    cp /myapp/init/database.yml /myapp/config/database.yml &&
+    rake db:create
 fi
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
